cd5948e3cbd175d2d15c6735799be831c9df91c5
ElisaSPereira <pereiraelisa365@gmail.com>
2024-08-23 15:09:56 -0300

    Atualização-Interclasse

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -6,6 +6,7 @@ import './styles.css';
 const Navigation = () => {
   return (
     <nav className="home2">
+      <a href="https://www.figma.com/proto/cdvgl7qjxWab1GDAfLTVY4/trabalho?node-id=220-89&t=xtegbWA0H4O1XDZR-1" className="link">FIGMA</a>
       <a href="/horarios" className="link">HORÁRIOS</a>
       <a href="/modalidades" className="link">MODALIDADES</a>
       <a href="/direcionamento" className="link">
